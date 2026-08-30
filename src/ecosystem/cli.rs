47b94f458733982bee96559f1be3a97d33f4f44b
Stanley Masinde <stanmasinde@gmail.com>
2026-08-30 16:03:53 +0300

    chore: remove unnecessary assignment

--- src/ecosystem/cli.rs
+++ src/ecosystem/cli.rs
@@ -5,14 +5,11 @@ use clap::{arg, builder::EnumValueParser, command, value_parser};
 use crate::ecosystem::types::ReleaseKind;
 
 pub fn cli() -> clap::ArgMatches {
-    let matches = command!()
+    command!()
         .arg(arg!([kind] "The release version e.g major.").required(true).value_parser(EnumValueParser::<ReleaseKind>::new()))
         .arg(arg!(
             -r --repo <PATH> "Specify the git repo."
         ).value_parser(value_parser!(PathBuf)))
         .after_help("This util helps streamline the release process. Calling git release increments the tag.")
-        .get_matches();
-
-    matches
-
+        .get_matches()
 }
